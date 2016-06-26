24a9de618b4d6eee45a0af01c4f0d46e819d95aa
Luz <luzma_87@yahoo.com>
2016-06-26 16:29:14 -0500

    agregando spinner al webview

--- app/src/main/java/com/lzm/Cajas/WebViewFragment.java
+++ app/src/main/java/com/lzm/Cajas/WebViewFragment.java
@@ -2,11 +2,13 @@ package com.lzm.Cajas;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class WebViewFragment extends Fragment {
     private static final String URL = "url";
@@ -39,8 +41,25 @@ public class WebViewFragment extends Fragment {
         context.setActiveFragment(MainActivity.FRAGMENT_WEB_VIEW);
 
         View view = inflater.inflate(R.layout.web_view_fragment, container, false);
-        WebView myWebView = (WebView) view.findViewById(R.id.webview);
-        myWebView.loadUrl(url);
+        final WebView webView = (WebView) view.findViewById(R.id.webview);
+
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                webView.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                webView.setVisibility(View.VISIBLE);
+            }
+
+        });
+
+        webView.loadUrl(url);
         return view;
     }
 
@@ -53,4 +72,5 @@ public class WebViewFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
     }
+
 }
